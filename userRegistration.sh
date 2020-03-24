77a0713f217c109e2c0b186cfb4d30aab09ad7cb
Hrishikesh <39852586+rishibodake@users.noreply.github.com>
2020-03-24 13:00:50 +0530

    Added:Last Name Check and Choice For User

--- userRegistration.sh
+++ userRegistration.sh
@@ -4,21 +4,46 @@
 #Author:Hrishikesh Manohar Bodake
 #Since:24 March 2020
 clear
+FIRSTNAME=1
+LASTNAME=2
 checkFirstName()  
 {
-   read -p "Enter Valid The FirstName:" firstName
-   pattern="^[A-Z][a-zA-Z]{3,}$"   #minimum 3 character and starts with uppercase
-   checkPattern=$firstName 
+   	read -p "Enter Valid The FirstName:" firstName
+   	pattern="^[A-Z][a-zA-Z]{3,}$"   #minimum 3 character and starts with uppercase
+   	checkPattern=$firstName 
+}
+checkLastName()
+{
+	read -p "Enter Valid The LastName:" lastName
+   	pattern="^[A-Z][a-zA-Z]{3,}$"   #minimum 3 character and starts with uppercase
+   	checkPattern=$firstName 
+	checkPattern=$lastName
 }
 checkPattern() 
 {
-   if [[ $checkPattern  =~ $pattern ]]
-   then
-      echo  Valid FirstName
-   else
-      echo Invalil FirstNAme
-      checkFirstName	
-   fi
+   	if [[ $checkPattern  =~ $pattern ]]
+   	then
+      		echo  Valid Input
+   	else
+      		echo Invalil Input
+  	 fi
 }
-checkFirstName
-checkPattern
+choose()
+{
+	printf "1: For FirstName/n"
+	printf "2: For LastName/n"
+	read choice
+	case $choice in
+			$FIRSTNAME)
+				  checkFirstName
+				  checkPattern;;
+			$LASTNAME)
+				  checkLastName
+				  checkPattern;;
+				*)
+				  printf "Wrong Choice"
+	esac
+}
+
+choose
+
