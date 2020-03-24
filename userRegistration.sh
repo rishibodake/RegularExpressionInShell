54f3f695214b9e58ffd1ce40a2763331723affaa
Hrishikesh <39852586+rishibodake@users.noreply.github.com>
2020-03-24 12:35:45 +0530

    Added:Valid First Name Validation

---
+++ userRegistration.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+
+#Discription:Check Valid User Registration Patterns
+#Author:Hrishikesh Manohar Bodake
+#Since:24 March 2020
+clear
+checkFirstName()  
+{
+   read -p "Enter Valid The FirstName:" firstName
+   pattern="^[A-Z][a-zA-Z]{3,}$"   #minimum 3 character and starts with uppercase
+   checkPattern=$firstName 
+}
+checkPattern() 
+{
+   if [[ $checkPattern  =~ $pattern ]]
+   then
+      echo  Valid FirstName
+   else
+      echo Invalil FirstNAme
+      checkFirstName	
+   fi
+}
+checkFirstName
+checkPattern
