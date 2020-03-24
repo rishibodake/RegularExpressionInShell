192e3f9e92218bdb0a190232490b5f4fb6641092
Hrishikesh <39852586+rishibodake@users.noreply.github.com>
2020-03-24 12:41:23 +0530

    Modified:GivenPerfectTabsForIdentation

--- userRegistration.sh
+++ userRegistration.sh
@@ -6,19 +6,19 @@
 clear
 checkFirstName()  
 {
-   read -p "Enter Valid The FirstName:" firstName
-   pattern="^[A-Z][a-zA-Z]{3,}$"   #minimum 3 character and starts with uppercase
-   checkPattern=$firstName 
+   	read -p "Enter Valid The FirstName:" firstName
+   	pattern="^[A-Z][a-zA-Z]{3,}$"   #minimum 3 character and starts with uppercase
+   	checkPattern=$firstName 
 }
 checkPattern() 
 {
-   if [[ $checkPattern  =~ $pattern ]]
-   then
-      echo  Valid FirstName
-   else
-      echo Invalil FirstNAme
-      checkFirstName	
-   fi
+  	 if [[ $checkPattern  =~ $pattern ]]
+   	then
+      		echo  Valid FirstName
+   	else
+      		echo Invalil FirstNAme
+      		checkFirstName	
+  	 fi
 }
 checkFirstName
 checkPattern
